8815c15aca13df01ae3bf7050b06f25e7307cd4d
Samuel Cooper <cooper.samuel@outlook.com>
2018-05-17 09:15:05 -0400

    about and skills

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -4,12 +4,15 @@ import './intro.scss';
 const Intro = (props) => {
     return (
         <div className="intro row align-items-center">
-            <div className="col">
+            <div className="col mb-5">
                 <div className="intro-title pb-5">
-                    Hey! I'm <strong>Sam</strong>,
+                    Hey! I'm <strong className="green">Sam</strong>,
                 </div>
                 <div className="intro-body">
-                    I'm a <strong>Fullstack Developer</strong>. I write software to do all kinds of stuff, everything from intuitive user experiences to complex algorithms.
+                    I'm a <strong className="green">Fullstack Developer</strong>. I write software to do all kinds of stuff, everything from intuitive user experiences to complex algorithms.
+                </div>
+                <div className="intro-contact pt-5">
+                    Let's get in touch!
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Intro from './components/intro/intro';
+import About from './components/about/about';
+import Skills from './components/skills/skills';
 import './styles.scss';
 
 const App = () => {
     return (
         <div className="container-fluid">
             <div className="row justify-content-center">
-                <div className="col-10">
+                <div className="col-9">
                     <Intro />
+                    <About />
+                    <div className="spacer">&nbsp;</div>
+                    <Skills />
+                    <div className="spacer">&nbsp;</div>
                 </div>
             </div>
         </div>
