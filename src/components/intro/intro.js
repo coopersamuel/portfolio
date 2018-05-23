d6d477125dada83ff4ab74b9062b006c7c185a5b
Samuel Cooper <cooper.samuel@outlook.com>
2018-05-22 21:10:26 -0400

    colored icon links

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -22,9 +22,9 @@ const Intro = (props) => {
                         I'm a <strong className="green">Fullstack Developer</strong>. I write software to do all sorts of things, everything from intuitive user experiences to complex algorithms.
                     </div>
                     <div className="intro-contact pt-5">
-                        <a href="mailto:cooper.samuel22@gmail.com"><MdMailOutline className="mr-3" size={30} /></a>
-                        <a target="_blank" href="https://github.com/coopersamuel"><GoMarkGithub className="mr-3" size={27} /></a>
-                        <a target="_blank" href="https://www.linkedin.com/in/samuel-cooper-b81945a5/"><FaLinkedinSquare className="mr-3" size={28} /></a>
+                        <a className="icon-link" href="mailto:cooper.samuel22@gmail.com"><MdMailOutline className="mr-3" size={30} /></a>
+                        <a className="icon-link" target="_blank" href="https://github.com/coopersamuel"><GoMarkGithub className="mr-3" size={27} /></a>
+                        <a className="icon-link" target="_blank" href="https://www.linkedin.com/in/samuel-cooper-b81945a5/"><FaLinkedinSquare className="mr-3" size={28} /></a>
                     </div>
                 </div>
                 <div className={`scroll-indicator ${isScrolled ? 'hide' : ''}`}>
